33e67449d0d928373e03c033bad854bebd9cae6e
Joe oliveira <joeoliveira89@gmail.com>
2018-08-07 09:28:27 -0700

    v3
    New UI -- Will probably add ability to go to a different one since this one is not pretty
    
    Both FireRed and Emerald ini creation
    
    You can now add more locations for it to search for, simply edit the FRoffsets/EMoffsets files with the name and location you want the program to check.
    
    Technically faster ini creation, but whos gonna notice that anyways.
    
    Its cleaner than before?

---
+++ Advanced Offset v3/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Rom_Label = new System.Windows.Forms.Label();
+            this.Open_Button = new System.Windows.Forms.Button();
+            this.Export_Button = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.Offset_View = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Offset_View)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Rom_Label);
+            this.groupBox1.Controls.Add(this.Open_Button);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 71);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ROM Information";
+            // 
+            // Rom_Label
+            // 
+            this.Rom_Label.AutoSize = true;
+            this.Rom_Label.Location = new System.Drawing.Point(120, 30);
+            this.Rom_Label.Name = "Rom_Label";
+            this.Rom_Label.Size = new System.Drawing.Size(101, 17);
+            this.Rom_Label.TabIndex = 1;
+            this.Rom_Label.Text = "No ROM Open";
+            // 
+            // Open_Button
+            // 
+            this.Open_Button.Location = new System.Drawing.Point(7, 22);
+            this.Open_Button.Name = "Open_Button";
+            this.Open_Button.Size = new System.Drawing.Size(91, 33);
+            this.Open_Button.TabIndex = 0;
+            this.Open_Button.Text = "Open";
+            this.Open_Button.UseVisualStyleBackColor = true;
+            this.Open_Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Export_Button
+            // 
+            this.Export_Button.Location = new System.Drawing.Point(311, 34);
+            this.Export_Button.Name = "Export_Button";
+            this.Export_Button.Size = new System.Drawing.Size(114, 33);
+            this.Export_Button.TabIndex = 2;
+            this.Export_Button.Text = "Export PGE ini";
+            this.Export_Button.UseVisualStyleBackColor = true;
+            this.Export_Button.Click += new System.EventHandler(this.Export_Button_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 622);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(440, 25);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(198, 20);
+            this.toolStripStatusLabel1.Text = "Advanced Offset v3 by Joexv";
+            // 
+            // Offset_View
+            // 
+            this.Offset_View.AllowUserToAddRows = false;
+            this.Offset_View.AllowUserToDeleteRows = false;
+            this.Offset_View.AllowUserToOrderColumns = true;
+            this.Offset_View.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Offset_View.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Offset_View.Location = new System.Drawing.Point(12, 90);
+            this.Offset_View.Name = "Offset_View";
+            this.Offset_View.ReadOnly = true;
+            this.Offset_View.RowTemplate.Height = 24;
+            this.Offset_View.Size = new System.Drawing.Size(417, 532);
+            this.Offset_View.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 647);
+            this.Controls.Add(this.Offset_View);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.Export_Button);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Advanced Offset";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Offset_View)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label Rom_Label;
+        private System.Windows.Forms.Button Open_Button;
+        private System.Windows.Forms.Button Export_Button;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.DataGridView Offset_View;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Advanced Offset v3/Form1.cs
@@ -0,0 +1,261 @@
+﻿using IniParser;
+using IniParser.Model;
+using Microsoft.VisualBasic;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string filePath = "";
+        public static string fullPath = "";
+        public static string GameCode = "";
+        public static string fileName = "";
+        public static string ROM = "";
+        bool isFireRed = false;
+        bool isEmerald = false;
+        private FileIniDataParser fileIniData = new FileIniDataParser();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "GBA File (*.gba)|*.gba";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = ofd.FileName;
+
+                ROM = Path.GetFileNameWithoutExtension(filePath);
+                fullPath = ofd.FileName;
+                fileName = ofd.SafeFileName;
+                filePath = fullPath.Replace(fileName, "");
+                ofd.Dispose();
+
+                BinaryReader br = new BinaryReader(File.OpenRead(fullPath));
+                br.BaseStream.Seek(0xAC, SeekOrigin.Begin);
+                switch (Encoding.UTF8.GetString(br.ReadBytes(4)))
+                {
+                    case "BPEE":
+                        isFireRed = false;
+                        isEmerald = true;
+                        GameCode = "BPEE";
+                        break;
+                    case "BPRE":
+                        isFireRed = true;
+                        isEmerald = false;
+                        GameCode = "BPRE";
+                        break;
+                    default:
+                        isFireRed = true;
+                        isEmerald = false;
+                        break;
+                }
+                br.Close();
+
+                UpdateDataGrid();
+                
+            }
+        }
+
+        public void UpdateDataGrid()
+        {
+            DoubleBuffered(Offset_View, true);
+            Offset_View.DataSource = DataTable();
+        }
+
+        public static void DoubleBuffered(DataGridView dgv, bool setting)
+        {
+            Type dgvType = dgv.GetType();
+            PropertyInfo pi = dgvType.GetProperty("DoubleBuffered",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            pi.SetValue(dgv, setting, null);
+        }
+
+        public DataTable DataTable()
+        {
+            string Game;
+            if (isFireRed == true)
+                Game = "FRoffsets.txt";
+            else
+                Game = "EMoffsets.txt";
+            string fileName = Application.StartupPath + "\\" + Game;
+            DataTable dt = new DataTable();
+            dt.Columns.Add(" ");
+            dt.Columns.Add("Offset");
+            dt.Columns.Add("Location");
+
+            var lines = File.ReadLines(fileName);
+            foreach (var line in lines)
+            {
+                DataRow dr = dt.NewRow();
+                string Offset = line.Split('=').Last();
+                string Name = line.Split('=').First();
+                //MessageBox.Show(Offset);
+                dr[" "] = Name;
+                dr["Offset"] = ReadOffset(Offset);
+                dr["Location"] = Offset;
+
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
+
+        public void MakeINI()
+        {
+            string Game;
+            if (isFireRed == true)
+            {
+                ExtractINI("FireRed.ini");
+                Game = "FRoffsets.txt";
+            }
+            else
+            {
+                ExtractINI("Emerald.ini");
+                Game = "EMoffsets.txt";
+            }
+            string fileName = Application.StartupPath + "\\" + Game;
+
+
+
+            var lines = File.ReadLines(fileName);
+            foreach (var line in lines)
+            {
+                string Offset = line.Split('=').Last();
+                string Name = line.Split('=').First();
+                Offset = PGEoffset(Offset);
+                //MessageBox.Show(ROM + ".ini");
+                Write(ROM + ".ini", Name, Offset);
+            }
+
+            string Item = Interaction.InputBox("Amount of Items?", "", "377");
+            string Move = Interaction.InputBox("Amount of Moves?", "", "355");
+            string Pokemon = Interaction.InputBox("Amount of Pokemon?", "", "412");
+            string Abilities = Interaction.InputBox("Amount of Abilities?", "", "76");
+            string Evolution = Interaction.InputBox("Amount of Evolutions Per Mon?", "", "5");
+            string TM = Interaction.InputBox("Amount of TMs?", "", "50");
+
+            Write(ROM + ".ini", "NumberOfItems", Item);
+            Write(ROM + ".ini", "NumberOfAttacks", Move);
+            Write(ROM + ".ini", "NumberOfAbilities", Abilities);
+            Write(ROM + ".ini", "NumberOfEvolutions", Evolution);
+            Write(ROM + ".ini", "NumberOfPokemon", Pokemon);
+            Write(ROM + ".ini", "TotalTMs", TM);
+
+        }
+
+        public string ReadOffset(string Offset)
+        {
+            long Long = Convert.ToInt64(Offset, 16);
+            BinaryReader br = new BinaryReader(File.OpenRead(fullPath));
+            br.BaseStream.Seek(Long, SeekOrigin.Begin);
+            byte[] bytes = br.ReadBytes(8);
+            int vOut = BitConverter.ToInt32(bytes, 0);
+            int vOut2 = vOut - 0x8000000;
+            string result = "0x" + vOut2.ToString("x7").ToUpper();
+            return result;
+        }
+
+        public string PGEoffset(string Offset)
+        {
+            long Long = Convert.ToInt64(Offset, 16);
+            BinaryReader br = new BinaryReader(File.OpenRead(fullPath));
+            br.BaseStream.Seek(Long, SeekOrigin.Begin);
+            byte[] bytes = br.ReadBytes(8);
+            int vOut = BitConverter.ToInt32(bytes, 0);
+            int vOut2 = vOut - 0x8000000;
+            string result = vOut2.ToString("x7").ToUpper();
+            return result;
+        }
+
+        public string Get_Data(string ROM, string Object)
+        {
+            string FileLocation = filePath + ROM + ".ini";
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile(FileLocation);
+            Object = data[GameCode][Object];
+            return Object;
+        }
+
+        public void Write(string ROM, string Object, string Value)
+        {
+            string FileLocation = filePath + ROM;
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile(FileLocation);
+            data[GameCode][Object] = NullCheck(Value);
+            parser.WriteFile(FileLocation, data);
+        }
+
+        public string NullCheck(string Object)
+        {
+            if (Object == "")
+                Object = "000000";
+
+            return Object;
+        }
+
+        private static void Extract(string nameSpace, string outDirectory, string internalFilePath, string resourceName)
+        {
+
+                Assembly assembly = Assembly.GetCallingAssembly();
+
+                using (Stream s = assembly.GetManifestResourceStream(nameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName))
+                using (BinaryReader r = new BinaryReader(s))
+                using (FileStream fs = new FileStream(outDirectory, FileMode.OpenOrCreate))
+                using (BinaryWriter w = new BinaryWriter(fs))
+                    w.Write(r.ReadBytes((int)s.Length));
+
+        }
+
+        private static void ExtractOffset(string FileName)
+        {
+            File.Delete(Application.StartupPath + "\\" + FileName);
+            Extract("AdvOffset", Application.StartupPath + "\\" + FileName, "Offsets", FileName);
+        }
+
+        private static void ExtractINI(string FileName)
+        {
+            if (File.Exists(filePath + ROM + ".ini"))
+            {
+                DialogResult dialogResult = MessageBox.Show("The INI already exists do you want to overwrite it?", "Warning", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Extract("AdvOffset", filePath + ROM + ".ini", "Offsets", FileName);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    //do something else
+                }
+            }
+            else
+                Extract("AdvOffset", filePath + ROM + ".ini", "Offsets", FileName);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ExtractOffset("EMoffsets.txt");
+        }
+
+        private void Export_Button_Click(object sender, EventArgs e)
+        {
+            MakeINI();
+        }
+    }
+}
